8cb451e2766c3c93db5e786c6eafea0daed10885
蒋勇 <jiangy@highzap.com>
2024-04-28 21:06:08 +0800

    chore: 增加页面布局测试

---
+++ pages/game.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <el-container>
+    <el-aside>
+      <Tree />
+    </el-aside>
+    <el-main>
+      <Table />
+    </el-main>
+  </el-container>
+</template>
