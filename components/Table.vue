8e8dbf87615d43a5fe1478ae9e9cbc1852c41c2f
蒋勇 <jiangy@highzap.com>
2024-04-28 23:36:48 +0800

    chore: 添加 Game Server 布局

--- components/Table.vue
+++ components/Table.vue
@@ -1,32 +1,54 @@
 <template>
-  <el-table :data="tableData" border style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+  <el-table
+    ref="tableRef"
+    :data="regions"
+    row-key="id"
+    border
+    default-expand-all
+    style="width: 100%"
+    @row-click="handleRowClick"
+  >
+    <el-table-column prop="value" label="名称" />
+    <el-table-column prop="initial" width="100" label="首字母" />
+    <el-table-column prop="name" width="180" label="类型" />
+    <el-table-column prop="status" width="100" label="状态" />
+    <el-table-column prop="sort" width="100" label="排序" />
+    <el-table-column prop="createdAt" width="100" label="创建时间" />
   </el-table>
 </template>
 
 <script lang="ts" setup>
-const tableData = [
-  {
-    date: "2016-05-03",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-02",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-04",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-  {
-    date: "2016-05-01",
-    name: "Tom",
-    address: "No. 189, Grove St, Los Angeles",
-  },
-];
+import serverData, { diablo4 } from "./server";
+const props = defineProps(["game"]);
+const regions = ref();
+const tableRef = ref();
+
+const handleRowClick = (row: any) => {
+  tableRef.value?.toggleRowExpansion(row);
+};
+
+watch(
+  () => props.game,
+  () => {
+    console.log("game", props.game);
+    if (props.game === "Diablo 4") {
+      regions.value = diablo4.tradeEnvironments.filter(
+        (m) => m.name === "Game Mode"
+      );
+    } else {
+      regions.value = serverData.tradeEnvironments.filter(
+        (m) => m.name === "Region"
+      );
+    }
+    regions.value.forEach((m) => {
+      if (m.childTradeEnvironments) {
+        m.children = [
+          ...m.childTradeEnvironments.filter((n) => n.name === "Realm"),
+        ];
+      }
+    });
+  }
+);
+
+console.log("regions", regions);
 </script>
--- pages/game.vue
+++ pages/game.vue
@@ -1,12 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const game = ref("");
+const handleGame = (name: string) => {
+  game.value = name;
+};
+</script>
 
 <template>
-  <el-container>
-    <el-aside>
-      <Tree />
+  <el-container style="height: 500px; padding: 24px">
+    <el-aside style="background: red">
+      <el-scrollbar>
+        <GameList @active-game="handleGame" />
+      </el-scrollbar>
     </el-aside>
-    <el-main>
-      <Table />
+    <el-main style="background: blue">
+      <el-scrollbar>
+        <Table :game="game" />
+      </el-scrollbar>
     </el-main>
   </el-container>
 </template>
